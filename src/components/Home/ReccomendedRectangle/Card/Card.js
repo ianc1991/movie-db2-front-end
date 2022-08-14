0aeef1383d70e17dc1f498b2d6efb8e624706480
Ian Christian <ianmchristian@gmail.com>
2022-08-14 15:57:03 -0400

    Added card flip

--- src/components/Home/ReccomendedRectangle/Card/Card.js
+++ src/components/Home/ReccomendedRectangle/Card/Card.js
@@ -12,24 +12,33 @@ const Card = (props) => {
   const LoadingIndicator = props => {
       const { promiseInProgress } = usePromiseTracker();
       return (
-        promiseInProgress && 
+        promiseInProgress &&
         <Loading />
-      );  
+      );
     }
 
   return (
     <div className='mainCardContainer'>
-          <LoadingIndicator />
-          {props.moviesArray.map((movie) => (
-              <button className="card cardCard" key={movie._id} tabIndex={0} onClick={()=>navigate(`/moviedetails?id=${movie._id}`)}>
-                  <img src={movie.poster} className="card-img-top cardImg" alt={ movie.title + " movie poster"} />
-                  <div className="home-card-body">
-                      <p className="card-text">{movie.title}</p>
-                      <FontAwesomeIcon icon={faStar} inverse /> {movie.imdb.rating}
-                  </div>
-              </button>
-          ))}
-        </div>
+      <LoadingIndicator />
+      {props.moviesArray.map((movie) => (
+        <div className='flipCard'>
+          <div className='cardInner'>
+            <div className='cardFront'>
+                <button className="card cardCard" key={movie._id} tabIndex={0} onClick={()=>navigate(`/moviedetails?id=${movie._id}`)}>
+                    <img src={movie.poster} className="card-img-top cardImg" alt={ movie.title + " movie poster"} />
+                    <div className="home-card-body">
+                        <p className="card-text">{movie.title}</p>
+                        <FontAwesomeIcon icon={faStar} inverse /> {movie.imdb.rating}
+                    </div>
+                </button>
+            </div>
+            <div className='cardBack' onClick={()=>navigate(`/moviedetails?id=${movie._id}`)}>
+              <p>{movie.plot}</p>
+            </div>
+          </div>
+      </div>
+      ))}
+    </div>
   )
 }
 
